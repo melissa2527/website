f9f5da018e1eb03167e96744f602ce2842849616
annie2527 <you@example.com>
2021-03-21 21:50:17 +0100

    remove outline contact form

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const MY_SEO = {
+    title: 'Melissa Genger',
+    description: 'Web developer with JavaScript, React, CSS/SASS, PHP, Laravel and MySQL experience',
+    openGraph: {
+        type: 'website',
+        url: 'https://www.melissagenger.com',
+        title: 'Melissa Genger',
+        description: 'Web developer with JavaScript, React, CSS/SASS, PHP, Laravel and MySQL experience',
+        image: 'img/long.png',
+    }
+};
